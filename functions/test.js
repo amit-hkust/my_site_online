d39ddf731a2afcfd599b68fa9cd302aab0113ae2
amit-hkust <amit.ab87@gmail.com>
2021-06-12 22:11:26 +0800

    test

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,16 @@
-exports.handler = async function(event, context,callback) {
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    })
-}
+const fetch = require("node-fetch");
+const API_ENDPOINT = 'https://ipinfo.io/json';
+exports.handler = async (event, context) => {
+    try {
+      const response = await fetch(API_ENDPOINT);
+      const data = await response.json();
+      return { statusCode: 200, body: JSON.stringify({ data }) };
+    } catch (error) {
+      console.log(error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Failed fetching data' }),
+      };
+    }
+  };
+            
